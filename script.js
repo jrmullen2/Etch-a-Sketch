c11a2569f66ffdf308ad4546619c23cee351fbf5
Jackson <jrmullen2@users.noreply.github.com>
2023-02-14 15:17:11 -0800

    Create for loop in script that creates required amount of divs

---
+++ script.js
@@ -0,0 +1,9 @@
+const body = document.querySelector("body");
+for(let i = 0; i < 16; i++)
+{
+    for(let j = 0; j < 16; j++)
+    {
+        const newDiv = document.createElement("div");
+        body.appendChild(newDiv)
+    }
+}
