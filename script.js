9bcc4a0bb36bdfc47c0d1c721563ffb275b058ee
Jackson <jrmullen2@users.noreply.github.com>
2023-02-15 16:20:39 -0800

    Add event listeners to each div. Create element inside each div so divs are not empty

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ for(let i = 0; i < 16; i++)
 {
     for(let j = 0; j < 16; j++)
     {
-        const newDiv = document.createElement("div");
-        body.appendChild(newDiv)
+        let newDiv = document.createElement("div");
+        let p = document.createElement("p");
+        newDiv.appendChild(p);
+        newDiv.classList.add("hover");
+        body.appendChild(newDiv);
     }
 }
+const hover = document.querySelectorAll(".hover");
+hover.forEach((div) => {
+    div.addEventListener('mouseenter', () => {
+        div.style.backgroundColor = "black";
+    });
+});
