0f4180f749ce200799e1468b82b1dad60bbb2e06
Jackson <jrmullen2@users.noreply.github.com>
2023-02-16 17:29:20 -0800

    Create prompt for user input. Create removeGrid anduserGrid functions and write implementations for these functions. Add syntax for function calls

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
 const body = document.querySelector("body");
+
 for(let i = 0; i < 16; i++)
 {
     for(let j = 0; j < 16; j++)
     {
         let newDiv = document.createElement("div");
+
         let p = document.createElement("p");
+
         newDiv.appendChild(p);
+
         newDiv.classList.add("hover");
+
         body.appendChild(newDiv);
     }
 }
+
 const hover = document.querySelectorAll(".hover");
+
 hover.forEach((div) => {
     div.addEventListener('mouseenter', () => {
         div.style.backgroundColor = "black";
     });
 });
+
+let userNumber = prompt("How many squares per side would you like? The maximum amount is 100.");
+
+function removeGrid()
+{
+    for(let g = 0; g < 16; g++)
+    {
+        for(let h = 0; h < 16; h++)
+        {
+            body.removeChild(newDiv);
+        }
+    }
+}
+function userGrid(number)
+{
+    for(let k = 0; k < number; k++)
+    {
+        for(let h = 0; h < number; h++)
+        {
+            let userDiv = document.createElement("div");
+            let pUser = document.createElement("p");
+            userDiv.appendChild(pUser);
+            userDiv.classList.add("hover");
+            body.appendChild(newDiv);
+        }   
+    }
+}
+removeGrid();
+userGrid(userNumber);
