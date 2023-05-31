2daa32e33170767969d7ad71795261db2e41fddf
Jackson Mullen <jrmullen2@icloud.com>
2023-05-31 10:00:00 -0700

    Make slight modifications to styling of code in script.js. Add to description of project in README.md

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Etch-a-Sketch
-A browser version of something between a sketchpad and an Etch-A-Sketch.
+
+A browser version of something between a sketchpad and an Etch-A-Sketch. This project displays basic skills of html, css, javascript, and css grid.
--- script.js
+++ script.js
@@ -2,60 +2,41 @@ const container = document.querySelector("#container");
 
 let containerChildren = [];
 
-let isBlack = true;
-
-let isColor = false;
-
 //Creates Original 16 by 16 grid
-
 for (let i = 0; i < 256; i++) {
   let newDiv = document.createElement("div");
-
   containerChildren[i] = newDiv;
-
   let p = document.createElement("p");
-
   newDiv.appendChild(p);
-
   newDiv.classList.add("hover");
-
   container.appendChild(newDiv);
 }
 
-//Making each div change background color when hovered over
-
+//Making each div change background color when hovered over (default effect)
 let hover = document.querySelectorAll(".hover");
 
 hover.forEach((div) => {
   div.addEventListener("mouseenter", () => {
     let redValue = Math.floor(Math.random() * 256);
-
     let blueValue = Math.floor(Math.random() * 256);
-
     let greenValue = Math.floor(Math.random() * 256);
-
     div.style.backgroundColor = `rgb(${redValue}, ${blueValue}, ${greenValue}`;
   });
 });
 
-//Providing button with functionality
-
+//Providing buttons with functionality
 const makeButton = document.getElementById("make");
 
 makeButton.addEventListener("click", () => {
   let userPrompt = prompt(
     "How many squares per side would you like? The maximum amount is 100. (Enter a number)"
   );
-
   userNumber = parseInt(userPrompt);
-
   if (userNumber > 100 || userNumber < 1 || isNaN(userNumber)) {
     alert("This is an invalid value. Please try again");
-
     return;
   }
   removeGrid();
-
   createGrid(userNumber);
 });
 
@@ -76,11 +57,8 @@ colorButton.addEventListener("click", () => {
     div.style.backgroundColor = "white";
     div.addEventListener("mouseenter", () => {
       let redValue = Math.floor(Math.random() * 256);
-
       let blueValue = Math.floor(Math.random() * 256);
-
       let greenValue = Math.floor(Math.random() * 256);
-
       div.style.backgroundColor = `rgb(${redValue}, ${blueValue}, ${greenValue}`;
     });
   });
@@ -95,46 +73,28 @@ function removeGrid() {
 
 function createGrid(number) {
   //Changes the number of rows and columns in grid
-
   document.getElementById(
     "container"
   ).style.gridTemplateColumns = `repeat(${number}, auto)`;
-
   document.getElementById(
     "container"
   ).style.gridTemplateRows = `repeat(${number}, auto)`;
-
   for (let k = 0; k < number ** 2; k++) {
     let userDiv = document.createElement("div");
-
     containerChildren[k] = userDiv;
-
     let pUser = document.createElement("p");
-
     userDiv.appendChild(pUser);
-
     userDiv.classList.add("hover");
-
     container.appendChild(userDiv);
   }
-
   hover = document.querySelectorAll(".hover");
-
-  console.log(hover.length);
-
   hover.forEach((div) => {
     div.addEventListener("mouseenter", () => {
       let redValue = Math.floor(Math.random() * 256);
-
       let blueValue = Math.floor(Math.random() * 256);
-
       let greenValue = Math.floor(Math.random() * 256);
-
       div.style.backgroundColor = `rgb(${redValue}, ${blueValue}, ${greenValue}`;
-
-      document.div.style.filter = `brightness(${brightnessPercent}%)`;
     });
   });
-
   return;
 }
