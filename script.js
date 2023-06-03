152f434625665de0bcc5c7cdef0be450c3c12906
Jackson Mullen <jrmullen2@icloud.com>
2023-06-03 15:52:41 -0700

    Modify event listener functions for buttons as well as createGrid function. Take beginning code in script.js and put it into a function called originalGrid. Add, provide functionality for,  and style a newButton called colorFade

--- script.js
+++ script.js
@@ -2,24 +2,20 @@ const container = document.querySelector("#container");
 
 let containerChildren = [];
 
-//Creates Original 16 by 16 grid
-for (let i = 0; i < 256; i++) {
-  let newDiv = document.createElement("div");
-  containerChildren[i] = newDiv;
-  let p = document.createElement("p");
-  newDiv.appendChild(p);
-  newDiv.classList.add("hover");
-  container.appendChild(newDiv);
+let userNumber = 16;
+
+function originalGrid(userNum) {
+  createGrid(userNum);
+  //Making each div change background color when hovered over (default effect)
+  let hover = document.querySelectorAll(".hover");
+  hover.forEach((div) => {
+    div.addEventListener("mouseenter", () => {
+      div.style.backgroundColor = "black";
+    });
+  });
 }
 
-//Making each div change background color when hovered over (default effect)
-let hover = document.querySelectorAll(".hover");
-
-hover.forEach((div) => {
-  div.addEventListener("mouseenter", () => {
-    div.style.backgroundColor = "black";
-  });
-});
+originalGrid(userNumber);
 
 //Providing buttons with functionality
 const makeButton = document.getElementById("make");
@@ -35,23 +31,36 @@ makeButton.addEventListener("click", () => {
   }
   removeGrid();
   createGrid(userNumber);
+  let hover = document.querySelectorAll(".hover");
+  hover.forEach((div) => {
+    div.addEventListener("mouseenter", () => {
+      div.style.backgroundColor = "black";
+    });
+  });
 });
 
 const blackButton = document.getElementById("black");
 
 const colorButton = document.getElementById("color");
 
+const colorFade = document.getElementById("colorFade");
+
 blackButton.addEventListener("click", () => {
+  removeGrid();
+  createGrid(userNumber);
+  let hover = document.querySelectorAll(".hover");
   hover.forEach((div) => {
-    div.style.backgroundColor = "white";
     div.addEventListener("mouseenter", () => {
       div.style.backgroundColor = "black";
     });
   });
 });
+
 colorButton.addEventListener("click", () => {
+  removeGrid();
+  createGrid(userNumber);
+  let hover = document.querySelectorAll(".hover");
   hover.forEach((div) => {
-    div.style.backgroundColor = "white";
     div.addEventListener("mouseenter", () => {
       let redValue = Math.floor(Math.random() * 256);
       let blueValue = Math.floor(Math.random() * 256);
@@ -61,7 +70,33 @@ colorButton.addEventListener("click", () => {
   });
 });
 
+colorFade.addEventListener("click", () => {
+  removeGrid();
+  createGrid(userNumber);
+  let hover = document.querySelectorAll(".hover");
+  hover.forEach((div) => {
+    div.style.backgroundColor = "white";
+    div.style.filter = "brightness(1)";
+    let brightnessTracker = 1.1;
+    let isColored = false;
+    div.addEventListener("mouseenter", () => {
+      if (isColored === false) {
+        let redValue = Math.floor(Math.random() * 256);
+        let blueValue = Math.floor(Math.random() * 256);
+        let greenValue = Math.floor(Math.random() * 256);
+        div.style.backgroundColor = `rgb(${redValue}, ${blueValue}, ${greenValue}`;
+        isColored = true;
+      }
+      if (brightnessTracker > 0) {
+        brightnessTracker -= 0.1;
+      }
+      div.style.filter = `brightness(${brightnessTracker})`;
+    });
+  });
+});
+
 function removeGrid() {
+  let hover = document.querySelectorAll(".hover");
   for (let g = 0; g < hover.length; g++) {
     container.removeChild(containerChildren[g]);
   }
@@ -84,11 +119,5 @@ function createGrid(number) {
     userDiv.classList.add("hover");
     container.appendChild(userDiv);
   }
-  hover = document.querySelectorAll(".hover");
-  hover.forEach((div) => {
-    div.addEventListener("mouseenter", () => {
-      div.style.backgroundColor = "black";
-    });
-  });
   return;
 }
